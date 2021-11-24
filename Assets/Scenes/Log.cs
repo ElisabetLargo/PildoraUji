df17fcb54def2e6e1114538195212318f4708540
Elisabet Largo <elisabet.largo@starloopstudios.com>
2021-11-24 18:22:30 +0100

    First version of log

---
+++ Assets/Scenes/Log.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("First branch log");
+    }
+}
